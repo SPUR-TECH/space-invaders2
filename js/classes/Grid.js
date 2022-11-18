bcf1a59c5be4436243866060670fc7e72085e776
Scott Atkinson <scott.atkin79@gmail.com>
2022-11-18 19:23:52 +0000

    move player up, change grid columwidth and place button around direction img

--- js/classes/Grid.js
+++ js/classes/Grid.js
@@ -15,7 +15,7 @@ class Grid {
     const columns = Math.floor(Math.random() * 10 + 5)
     const rows = Math.floor(Math.random() * 5 + 2)
 
-    this.width = columns * 30
+    this.width = columns * 48
 
     for (let x = 0; x < columns; x++) {
       for (let y = 0; y < rows; y++) {
--- js/classes/Player.js
+++ js/classes/Player.js
@@ -18,7 +18,7 @@ class Player {
       this.height = image.height * scale
       this.position = {
         x: canvas.width / 2 - this.width / 2,
-        y: canvas.height - this.height - 60
+        y: canvas.height - this.height - 80
       }
     }
 
