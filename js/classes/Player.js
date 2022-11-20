b690b063f6fd4bd9dad96528d90f8bbf89d02d5c
Scott Atkinson <scott.atkin79@gmail.com>
2022-11-20 14:19:26 +0000

    move player up to avoid buttons for smaller screens

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -18,7 +18,7 @@ class Player {
       this.height = image.height * scale
       this.position = {
         x: canvas.width / 2 - this.width / 2,
-        y: canvas.height - this.height - 110
+        y: canvas.height - this.height - 120
       }
     }
 
